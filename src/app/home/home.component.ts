e9afca9bd39ffc080548310adb38c1a4c684ff32
Uzair Iqbal <uzair.qq@outlook.com>
2018-01-27 12:51:12 +0500

    used two binding using ngModel to call a f

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,5 +8,12 @@ import { Employee } from '../models/employee.model';
 })
 export class HomeComponent {
   languages=['English','Spanish','Hindi','Urdu'];
-  model=new Employee('uzair','laraib')
+  model=new Employee('uzair','laraib');
+
+  firstNameToUpperCase(value:string){
+    if(value.length>0)
+     this.model.firstName=value.charAt(0).toUpperCase() + value.slice(1);
+    else
+    this.model.firstName=value;
+}
 }

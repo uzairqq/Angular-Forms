20fed7e64f8a62b0d360003b85702592fa5c1df0
Uzair Iqbal <uzair.qq@outlook.com>
2018-01-28 05:22:56 +0500

    added validation on selector

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import {Component} from '@angular/core';
 import { Employee } from '../models/employee.model';
+import { Console } from '@angular/core/src/console';
 
 @Component({
   selector: 'home',
@@ -8,7 +9,16 @@ import { Employee } from '../models/employee.model';
 })
 export class HomeComponent {
   languages=['English','Spanish','Hindi','Urdu'];
-  model=new Employee('uzair','laraib',true,"w2",'English');
+  model=new Employee('uzair','laraib',true,"w2",'default');
+  hasPrimaryLanguageError=false;
+
+  ValidatePrimaryLanguage($event){
+    // console.log('lang: ' + this.model.primaryLanguage);
+    if(this.model.primaryLanguage === 'default')
+    this.hasPrimaryLanguageError=true;
+    else
+    this.hasPrimaryLanguageError=false;
+  }
 
   
 
